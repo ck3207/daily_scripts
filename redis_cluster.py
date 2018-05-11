23f297ae714f4c37dc818c36079a18d3851c96bb
ck3207 <31480811+ck3207@users.noreply.github.com>
2018-05-11 18:07:56 +0800

    新增redis集群操作脚本

---
+++ redis_cluster.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+__author__ = "chenk"
+
+from rediscluster import StrictRedisCluster
+import sys
+
+def redis_cluster():
+    redis_nodes =  [{'host':'10.20.18.170','port':6380},
+                    {'host':'10.20.18.170','port':6381},
+                    {'host':'10.20.18.170','port':6382},
+                    {'host':'10.20.18.170','port':6383},
+                    {'host':'10.20.18.170','port':6384},
+                    {'host':'10.20.18.170','port':6385}
+                   ]
+    try:
+        redisconn = StrictRedisCluster(startup_nodes=redis_nodes,password="abc")
+    except Exception as e:
+        print("Connect Error!")
+        print(str(e))
+        sys.exit(1)
+
+    return redisconn
+    # redisconn.set('name','admin')
+    # redisconn.set('age',18)
+    # print("name is: ", redisconn.get('name'))
+    # print("age  is: ", redisconn.get('age'))
+
+redis = redis_cluster()
+fund_account_list = [30003590,30003592,30003593,30003594,30003595,30003596,30003598,30003599]
+# fund_account_list = [30,2,12,10]
+
+i = 1
+value = ""
+is_break = 0
+max_fund_account = sorted(fund_account_list,reverse=True)[0]
+while max_fund_account:
+    for fund_account in fund_account_list:
+        if fund_account == i:
+            value += "1"
+            is_break = 1
+            break
+    if is_break == 0:
+        value += "0"
+
+    i += 1
+    is_break = 0
+    max_fund_account -= 1
+
+redis.set("{ifs.management.base.service.redisclusterservice.bitmap}.auth.18511",value)
+print("Done")
